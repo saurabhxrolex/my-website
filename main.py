bfb67913d4c27b795b0f7edf88da838ebbf8186c
saurabhxrolex <rolexdlr53@gmail.com>
2026-07-04 06:11:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,3 +72,9 @@ def unbind(data: TokenRequest):
         "msg": "Email unbound successfully ✅",
         "token": token
     }
+import os
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
